acad5172dd724579eddc355fa4652a7d1577347e
talenthuntbz <talenthunt.bz@gmail.com>
2025-07-15 15:03:41 +0530

    tests added

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MainTest {
+
+    @Test
+    public void envVarTest(){
+
+        Assert.assertNotNull(System.getenv("LAST_MAVEN_COMMAND"));
+        System.out.println(System.getenv("LAST_MAVEN_COMMAND"));
+    }
+}
